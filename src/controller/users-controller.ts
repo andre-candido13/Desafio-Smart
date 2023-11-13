ee36a08aa235f9f9ed70eb0d47597da0bf94d1b7
andre candido <andre.eng1313@gmail.com>
2023-11-13 11:40:58 -0300

    feat: Controllers createUser

---
+++ src/controller/users-controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+
+
+
+export async function createUser (req: Request, res: Response) {
+
+    const { name, email, password } = req.body
+
+    try {
+
+
+    } catch ()e {
+
+    }
+
+}
