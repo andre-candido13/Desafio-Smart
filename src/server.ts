5290dc64ab012911751000ef981e49a10d706007
andre candido <andre.eng1313@gmail.com>
2023-11-13 11:13:18 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config()
+
+const server = express()
+server.use(cors())
+server.use(express.json())
+
+//routes
+
+
+
+
+server.listen(5000, () => console.log("Servidor ON na porta", + 5000))
